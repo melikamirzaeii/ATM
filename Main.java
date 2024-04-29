2ca9ac21ff5a108c7733105bcb666b43363a6f09
unknown <melikamz1984@gmail.com>
2024-04-29 23:31:34 +0330

    commit

---
+++ Main.java
@@ -0,0 +1,212 @@
+/*sanfoundry.com*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Main extends JFrame{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ATM");
+        frame.setSize(600, 500);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel entekhabzaban = new JPanel();
+        entekhabzaban.setLayout(null);
+        entekhabzaban.setBounds(0,0,600,500);
+        entekhabzaban.setBackground(Color.lightGray);
+        JPanel enterpassword = new JPanel();
+        enterpassword.setLayout(null);
+        enterpassword.setBounds(0,0,600,500);
+        enterpassword.setBackground(Color.lightGray);
+        JPanel tarakonesh = new JPanel();
+        tarakonesh.setLayout(null);
+        tarakonesh.setBounds(0,0,600,500);
+        tarakonesh.setBackground(Color.lightGray);
+        JPanel changepassword = new JPanel();
+        changepassword.setLayout(null);
+        changepassword.setBounds(0,0,600,500);
+        changepassword.setBackground(Color.lightGray);
+        JPanel elammojodi = new JPanel();
+        elammojodi.setLayout(null);
+        elammojodi.setBounds(0,0,600,500);
+        elammojodi.setBackground(Color.lightGray);
+        JPanel bardasht = new JPanel();
+        bardasht.setLayout(null);
+        bardasht.setBounds(0,0,6,500);
+        bardasht.setBackground(Color.lightGray);
+        JPanel kartbekart = new JPanel();
+        kartbekart.setLayout(null);
+        kartbekart.setBounds(0,0,600,500);
+        kartbekart.setBackground(Color.lightGray);
+        JPanel natijeh = new JPanel();
+        natijeh.setLayout(null);
+        natijeh.setBounds(0,0,600,500);
+        natijeh.setBackground(Color.lightGray);
+        JLabel label = new JLabel("Choose Language     زبان خود را انتخاب کنید");
+        label.setBounds(200,75,450,250);
+        JButton en = new JButton("English");
+        en.setBounds(0,200,150,50);
+        JButton fa = new JButton("فارسی");
+        fa.setBounds(350,200,150,50);
+        JLabel label2 = new JLabel("Enter Password");
+        label2.setBounds(250,200,400,300);
+        JTextField passw = new JTextField(20);
+        passw.setSize(50,25);
+        passw.setEditable(true);
+        JButton conf = new JButton("Confirm");
+        conf.setBounds(200,350,250,50);
+        JButton passwch= new JButton("Change Password");
+        passwch.setBounds(50,100,150,55);
+        JButton depos = new JButton("Deposit");
+        depos.setBounds(50,300,150,55);
+        JButton bala = new JButton("Balance");
+        bala.setBounds(300,100,150,55);
+        JButton transf = new JButton("Transfer Money");
+        transf.setBounds(300,300,150,55);
+        JLabel newp = new JLabel("Enter New Password");
+        newp.setBounds(250,150,100,50);
+        JTextField npasswf = new JTextField(20);
+        npasswf.setEditable(true);
+        npasswf.setSize(100,25);
+        JButton confi = new JButton("Confirm");
+        confi.setBounds(400,300,100,25);
+        JLabel balance = new JLabel("Your Account Balance Is: 94850000");
+        balance.setBounds(250,150,200,100);
+        JLabel d = new JLabel("Enter The Amount of Money To Deposit");
+        d.setBounds(150,200,250,100);
+        JTextField money = new JTextField(20);
+        money.setEditable(true);
+        money.setSize(200,50);
+        JButton c = new JButton("Confirm");
+        c.setBounds(300,400,100,25);
+        JLabel amount = new JLabel("Enter The Amount of Money");
+        amount.setBounds(300,100,150,50);
+        JTextField amountf = new JTextField(20);
+        amountf.setEditable(true);
+        amountf.setSize(150,50);
+        JLabel acnu = new JLabel("Enter The Account Number");
+        acnu.setBounds(300,300,150,50);
+        JTextField acnuf = new JTextField(20);
+        acnuf.setEditable(true);
+        acnuf.setSize(150,50);
+        JButton co = new JButton("Confirm");
+        co.setBounds(300,450,100,25);
+        JLabel resu = new JLabel("The Action Was Completed Succesfully");
+        resu.setBounds(300,250,150,50);
+        en.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(entekhabzaban);
+                frame.add(enterpassword);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        fa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(entekhabzaban);
+                frame.add(enterpassword);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        conf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(enterpassword);
+                frame.add(tarakonesh);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        passwch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(tarakonesh);
+                frame.add(changepassword);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        depos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(tarakonesh);
+                frame.add(bardasht);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        bala.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(tarakonesh);
+                frame.add(elammojodi);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        transf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(tarakonesh);
+                frame.add(kartbekart);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        confi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(changepassword);
+                frame.add(natijeh);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(bardasht);
+                frame.add(natijeh);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        co.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(kartbekart);
+                frame.add(natijeh);
+                frame.revalidate();
+                frame.repaint();
+            }
+        });
+        entekhabzaban.add(label);
+        entekhabzaban.add(en);
+        entekhabzaban.add(fa);
+        enterpassword.add(label2);
+        enterpassword.add(passw);
+        enterpassword.add(conf);
+        tarakonesh.add(passwch);
+        tarakonesh.add(depos);
+        tarakonesh.add(bala);
+        tarakonesh.add(transf);
+        changepassword.add(newp);
+        changepassword.add(npasswf);
+        changepassword.add(confi);
+        elammojodi.add(balance);
+        bardasht.add(d);
+        bardasht.add(money);
+        bardasht.add(c);
+        kartbekart.add(amount);
+        kartbekart.add(amountf);
+        kartbekart.add(acnu);
+        kartbekart.add(acnuf);
+        kartbekart.add(co);
+        natijeh.add(resu);
+        frame.add(entekhabzaban);
+        frame.setVisible(true);
+    }
+
+    }
